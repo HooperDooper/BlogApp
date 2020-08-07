d989c814b52d0355733eadaf4bf1c1ba5428a57a
HooperDooper <moumen.houssem.bros@gmail.com>
2020-08-07 11:12:25 +0200

    First Commit

---
+++ src/build/precache-manifest.bfe6416a11fdb1b143e2debfbb64caf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb38909d5bc9964d294d89488a1be7ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "225df67215f5b1c84043",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "922aaebcb13b6cfee91a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "225df67215f5b1c84043",
+    "url": "/static/js/2.303ea01e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.303ea01e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "922aaebcb13b6cfee91a",
+    "url": "/static/js/main.b2dc239d.chunk.js"
+  },
+  {
+    "revision": "3da7549e4e9d06a97aae",
+    "url": "/static/js/runtime-main.24ee9f0c.js"
+  }
+]);
